5f5a27ee03cd1b38a17e68ea879e0550cef3fb82
Ian Wordes <iwordes@student.42.us.org>
2017-03-03 09:50:31 -0800

    staging -> stage

--- gitbar.sh
+++ gitbar.sh
@@ -23,12 +23,12 @@ ps1_git() {
 		modded=`echo $st | grep -c 'modified:'`
 		deleted=`echo $st | grep -c 'deleted:'`
 
-		echo -n " \e[1m|\e[0m \e[93m⇏ staging\e[0m"
+		echo -n " \e[1m|\e[0m \e[93m⇏ stage\e[0m"
 		[ $added -gt 0 ] && echo -n " \e[92m+$added\e[0m"
 		[ $modded -gt 0 ] && echo -n " \e[93m±$modded\e[0m"
 		[ $deleted -gt 0 ] && echo -n " \e[91m-$deleted\e[0m"
 	else
-		echo -n " \e[1m|\e[0m \e[92m⇒ staging\e[0m"
+		echo -n " \e[1m|\e[0m \e[92m⇒ stage\e[0m"
 	fi
 
 	if echo $st | grep -q 'Untracked files:'; then
