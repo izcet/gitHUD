32cbdf6db05a127df168c153f6098c48a38a8a0b
Ian Wordes <iwordes@student.42.us.org>
2017-03-02 19:25:22 -0800

    Add more specific staging info to gitbar

--- gitbar.sh
+++ gitbar.sh
@@ -16,8 +16,15 @@ ps1_git() {
 		echo -n " \e[1m|\e[0m \e[92m⇪ latest\e[0m"
 	fi
 
-	if echo $st | grep -q 'Changes not staged for commit'; then
+	if echo $st | egrep -q '^Changes .*commit.*'; then
+		added=`echo $st | grep -c 'new file:'`
+		modded=`echo $st | grep -c 'modified:'`
+		deleted=`echo $st | grep -c 'deleted:'`
+
 		echo -n " \e[1m|\e[0m \e[93m⇏ stage\e[0m"
+		[ $added -gt 0 ] && echo -n " \e[92m+$added\e[0m"
+		[ $modded -gt 0 ] && echo -n " \e[93m±$modded\e[0m"
+		[ $deleted -gt 0 ] && echo -n " \e[91m-$deleted\e[0m"
 	else
 		echo -n " \e[1m|\e[0m \e[92m⇒ stage\e[0m"
 	fi
